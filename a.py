8e9d3a6f96a7bb0a43f0cb9cd6c29f6153e9018f
TinaSsh <tina_atur@163.com>
2019-02-18 09:27:35 +0800

    add c.py, modify a.py use 'if conditon' and 'for loop'

--- a.py
+++ a.py
@@ -1,2 +1,9 @@
-123
-test
+for i in range(0,5):
+    print "hello world"
+
+h = ["a","b","c"]
+for i in h
+    print h[i]
+    for j in h
+        print h[j]
+    
---
+++ c.py
@@ -0,0 +1,5 @@
+a = input("please enter a:")
+if a >1 :
+    print "class 2"
+else :
+    print "class 1"
